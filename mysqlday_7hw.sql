18cfebaecb99c870517c73c73714cd0ca65df098
goutham v s <gouthamvs11@gmail.com>
2025-11-21 15:04:38 +0530

    day_7

---
+++ mysqlday_7hw.sql
@@ -0,0 +1,3 @@
+INSERT INTO users_table VALUES(1,'Raj','Chennai',88,5,'Fitness'),(2,'Anu','Mumbai',91,NULL,'Diet'),(3,'Ravi','Chennai',78,3,'Fitness'),(4,'Meena','Delhi',82,NULL,'Diet'),(5,'Farah','Mumbai',95,4,'Fitness'),(6,'Kiran','Pune',70,NULL,'Yoga'),(7,'Latha','Pune',87,NULL,'Fitness');
+SELECT * FROM users_table WHERE score > (SELECT AVG(score) FROM users_table);
+SELECT name FROM users_table WHERE challenge = (SELECT challenge FROM users_table WHERE name = 'Farah');
